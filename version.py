7d58f5192b6e71bd56339e96468f466a00ca123a
Yifu Yu <root@jackyyf.com>
2015-03-07 00:37:00 +0800

    Fix version.py wrong path.

--- version.py
+++ version.py
@@ -14,7 +14,7 @@ if len(sys.argv) != 3:
 	print >>sys.stderr, 'Usage: %s version_short version_full' % sys.argv[0]
 	sys.exit(1)
 
-with open('version.go', 'wb') as f:
+with open('minegate/version.go', 'wb') as f:
 	f.write(code_template.format(
 		version_short=sys.argv[1].replace('"', '\\"'),
 		version_full=sys.argv[2].replace('"', '\\"'),
